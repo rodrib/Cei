a72fe381c5cfb03392705d8f1cfab00ca2f6ef97
rodrib <rodribogado50@gmail.com>
2024-09-30 18:40:07 -0300

    update main

--- main.py
+++ main.py
@@ -1,56 +1,48 @@
-# import streamlit as st
 
-# def add_bg_from_local():
-#     st.markdown(
-#         f"""
-#         <style>
-#         .stApp {{
-#             background-image: url("webscraping.jpg");
-#             background-size: cover;
-#         }}
-#         </style>
-#         """,
-#         unsafe_allow_html=True
-#     )
-
-# add_bg_from_local()
-
-# st.markdown(add_bg_from_local(), unsafe_allow_html=True)
 
 import streamlit as st
 
-original_title = '<h1 style="font-family: serif; color:white; font-size: 20px;">Streamlit CSS Styling✨ </h1>'
-st.markdown(original_title, unsafe_allow_html=True)
+# original_title = '<h1 style="font-family: serif; color:white; font-size: 20px;">Comite de Etica Provincial✨ </h1>'
+# st.markdown(original_title, unsafe_allow_html=True)
 
 
-# Set the background image
-background_image = """
-<style>
-[data-testid="stAppViewContainer"] > .main {
-    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
-    background-position: center;  
-    background-repeat: no-repeat;
-}
-</style>
-"""
+# # Set the background image
+# background_image = """
+# <style>
+# [data-testid="stAppViewContainer"] > .main {
+#     background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+#     background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+#     background-position: center;  
+#     background-repeat: no-repeat;
+# }
+# </style>
+# """
 
-st.markdown(background_image, unsafe_allow_html=True)
+# st.markdown(background_image, unsafe_allow_html=True)
 
-st.text_input("", placeholder="Streamlit CSS ")
+# st.text_input("", placeholder="Streamlit CSS ")
 
-input_style = """
-<style>
-input[type="text"] {
-    background-color: transparent;
-    color: #a19eae;  // This changes the text color inside the input box
-}
-div[data-baseweb="base-input"] {
-    background-color: transparent !important;
-}
-[data-testid="stAppViewContainer"] {
-    background-color: transparent !important;
-}
-</style>
-"""
-st.markdown(input_style, unsafe_allow_html=True)
+# input_style = """
+# <style>
+# input[type="text"] {
+#     background-color: transparent;
+#     color: #a19eae;  // This changes the text color inside the input box
+# }
+# div[data-baseweb="base-input"] {
+#     background-color: transparent !important;
+# }
+# [data-testid="stAppViewContainer"] {
+#     background-color: transparent !important;
+# }
+# </style>
+# """
+# st.markdown(input_style, unsafe_allow_html=True)
+
+
+# Page title
+st.set_page_config(page_title='Analisis de datos del CENAGEN', page_icon='📊',
+                   layout="wide", # Forma de layout ancho o compacto
+                    initial_sidebar_state="expanded" # Definimos si el sidebar aparece expandido o colapsado
+                   )
+st.title('📊 Analisis de datos del IGeHM')
+
